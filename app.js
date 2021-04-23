a3039cd24228642bd85cf6b6f32dce6ca426536d
Miss-Coder-developer <misscoderexpert@gmail.com>
2021-04-23 16:51:42 +0400

    change image response

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const app = express();
 require('./config/config');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
+const path = require('path')
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/images'))
+
 app.use(require('./routes/admin').admin)
 
 // 404
-app.use((req, res)=>{
-    res.status(404).json({message:"Page Not found 404"});
-})
+// app.use((req, res)=>{
+//     res.status(404).json({message:"Page Not found 404"});
+// })
 
 module.exports = app;
