591e4332b3d36fed2c4893016c76a4fb16e46482
Aram <mirzoyanaram90@gmail.com>
2021-04-27 20:34:56 +0400

    add cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+var corsOptions = {
+    origin: function (origin, callback) {
+      // db.loadOrigins is an example call to load
+      // a list of origins from a backing database
+      db.loadOrigins(function (error, origins) {
+        callback(error, origins)
+      })
+    }
+  }
 require('./config/config');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
@@ -11,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/images'))
 
-app.use(require('./routes/admin').admin)
+app.use(require('./routes/admin').admin, cors(corsOptions))
 
 // 404
 // app.use((req, res)=>{
